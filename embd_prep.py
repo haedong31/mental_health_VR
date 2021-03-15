9517421dfab801db2f032e45aac124646a1ff920
Haedong Kim <haedong.kim90@gmail.com>
2021-03-15 13:31:20 -0400

    Data split

--- embd_prep.py
+++ embd_prep.py
@@ -1,8 +1,10 @@
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
 from bert_vocab_check import BertVocabCheck
 from expand_contraction import expand_contraction
 from glob import glob
+from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 import re
 import os
 
@@ -174,3 +176,82 @@ labels = [0]*len(con_utts) + [1]*len(exp_utts)
 df = pd.DataFrame(data={"utts": utts, "labels": labels})
 df = df.sample(frac = 1)
 df.to_csv("./data/cookie_ebd_prep.csv")
+
+## train-test split
+def trunc_pad(s, trunc_len):
+    # max length of sentences
+    max_len = 0
+    for v in s.values:
+        if len(v) > max_len:
+            max_len = len(v)
+    
+    # truncate sentences longer than `trunc_len`
+    if max_len > trunc_len:
+        long_sen_idx = list()
+        for n, v in enumerate(s.values):
+            if len(v) > trunc_len:
+                long_sen_idx.append(n)
+        
+        s = s.drop(s.index[long_sen_idx])
+        
+        # renew max length of sentences
+        max_len = 0
+        for v in s.values:
+            if len(v) > max_len:
+                max_len = len(v)
+    else:
+        long_sen_idx = None
+    
+    # zero padding
+    padded = np.array([v + [0]*(max_len - len(v)) for v in s.values])
+    
+    return padded, long_sen_idx
+
+# import dataset
+df = pd.read_csv("./data/cookie_ebd_prep.csv", index_col=0)
+df["labels"].value_counts()
+
+# load model and tokenizer
+model = BertModel.from_pretrained("bert-base-cased")
+tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+max_input_size = 512
+
+ # tokenization {101: [CLS], 102: [SEP]}
+tokenized = df["utts"].apply((lambda x: tokenizer.encode(x, add_special_tokens=True)))
+
+# sequence lengh distribution
+seq_lens = []
+for s in tokenized:
+    seq_lens.append(len(s))
+seq_lens = pd.Series(seq_lens)
+
+hist = seq_lens.hist()
+hist.set_title("Sentence lengths histogram")
+
+# truncate too long sentences and zero padding
+_, long_sen_idx = trunc_pad(tokenized, max_input_size)
+df.drop(df.iloc[long_sen_idx].index, inplace=True)
+
+df_con = df[df["labels"] == 0]
+df_exp = df[df["labels"] == 1]
+
+# train:valid:test = 0.765:0.135:0.1
+train_test_ratio = 0.90
+train_valid_ratio = 0.85
+
+df_con_train, df_con_test = train_test_split(df_con, train_size=train_test_ratio, random_state=1)
+df_exp_train, df_exp_test = train_test_split(df_exp, train_size=train_test_ratio, random_state=1)
+
+df_con_train, df_con_valid = train_test_split(df_con_train, train_size=train_valid_ratio, random_state=1)
+df_exp_train, df_exp_valid = train_test_split(df_exp_train, train_size=train_valid_ratio, random_state=1)
+
+df_train = pd.concat([df_con_train, df_exp_train], ignore_index=True, sort=False)
+df_valid = pd.concat([df_con_valid, df_exp_valid], ignore_index=True, sort=False)
+df_test = pd.concat([df_con_test, df_exp_test], ignore_index=True, sort=False)
+
+# write processed data
+destination_folder = "./"
+
+df_train.to_csv(destination_folder+"train.csv", index=False)
+df_valid.to_csv(destination_folder+"valid.csv", index=False)
+df_test.to_csv(destination_folder+"test.csv", index=False)
