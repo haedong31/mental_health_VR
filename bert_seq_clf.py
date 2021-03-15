13eeb2bbae149b43d2f2e2246608eb6242c3e0a1
Haedong Kim <haedong.kim90@gmail.com>
2021-03-15 15:12:19 -0400

    Saving results

--- bert_seq_clf.py
+++ bert_seq_clf.py
@@ -1,7 +1,8 @@
 import os
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import classification_report, confusion_matrix
+import pandas as pd
+from sklearn import metrics
 
 from transformers import BertTokenizer, BertForSequenceClassification
 from torchtext.data import Field, TabularDataset, BucketIterator, Iterator
@@ -14,18 +15,17 @@ import torch.optim as optim
 tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-max_seq_len = 128
 batch_size = 4
 pad_idx = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
 unk_idx = tokenizer.convert_tokens_to_ids(tokenizer.unk_token)
 
 source_folder = os.getcwd()
-destination_folder = os.getcwd()
+destination_folder = "./results/20210315"
 
 # define fields
 label_field = Field(sequential=False, use_vocab=False, batch_first=True, dtype=torch.float)
 text_field = Field(use_vocab=False, tokenize=tokenizer.encode, lower=False, include_lengths=False,
-    batch_first=True, fix_length=max_seq_len, pad_token=pad_idx, unk_token=unk_idx)
+    batch_first=True, pad_token=pad_idx, unk_token=unk_idx)
 fields = [("text", text_field), ("label", label_field)]
 
 # create tabular dataset
@@ -94,7 +94,7 @@ def load_metrics(load_path):
 
     return train_loss_list, valid_loss_list, global_steps_list
 
-def train_model(model, optimizer, file_path=destination_folder,
+def train_model(model, optimizer, base_file_name,
                 train_loader=train_iter, valid_loader=valid_iter, num_epochs=5,
                 eval_every=len(train_iter) // 2, best_valid_loss=float("Inf")):
     # initialize running values
@@ -104,6 +104,9 @@ def train_model(model, optimizer, file_path=destination_folder,
     train_loss_list = []
     valid_loss_list = []
     global_steps_list = []
+
+    model_file_path = os.path.join(destination_folder, base_file_name+"model.pt")
+    metirc_file_path = os.path.join(destination_folder, base_file_name+"metrics.pt")
 
     # training loop
     model.train()
@@ -163,13 +166,13 @@ def train_model(model, optimizer, file_path=destination_folder,
                 # check point
                 if best_valid_loss > avg_valid_loss:
                     best_valid_loss = avg_valid_loss
-                    save_checkpoint(os.path.join(file_path,"model.pt"), model, best_valid_loss)
-                    save_metrics(os.path.join(file_path,"metrics.pt"), train_loss_list, valid_loss_list, global_steps_list)
+                    save_checkpoint(model_file_path, model, best_valid_loss)
+                    save_metrics(metirc_file_path, train_loss_list, valid_loss_list, global_steps_list)
 
-    save_metrics(os.path.join(file_path,"metrics.pt"), train_loss_list, valid_loss_list, global_steps_list)
+    save_metrics(metirc_file_path, train_loss_list, valid_loss_list, global_steps_list)
     print("Training finished")
 
-def evaluate(model, test_loader=test_iter):
+def evaluate(model, base_file_name, test_loader=test_iter):
     y_pred = []
     y_true = []
 
@@ -187,34 +190,65 @@ def evaluate(model, test_loader=test_iter):
             y_true.extend(label.tolist())
 
     print("Classification report:")
-    print(classification_report(y_true, y_pred, labels=[1,0], digits=4))
+    print(metrics.classification_report(y_true, y_pred, labels=[1,0], digits=4))
 
-    cm = confusion_matrix(y_true, y_pred, labels=[1,0])
+    cm = metrics.confusion_matrix(y_true, y_pred, labels=[1,0])
+
     ax = plt.subplot()
-    sns.heatmap(cm, annot=True, ax=ax, cmap="Blues", fmt="d")
-
+    cm_plot = sns.heatmap(cm, annot=True, ax=ax, cmap="Blues", fmt="d")    
     ax.set_title("Confusion matrix")
     ax.set_xlabel("Predicted labels")
     ax.set_ylabel("True labels")
     ax.xaxis.set_ticklabels(["FAKE", "TRUE"])
     ax.yaxis.set_ticklabels(["FAKE", "TRUE"])
+    
+    fig = cm_plot.get_figure()
+    fig.savefig(os.path.join(destination_folder, base_file_name+"confusion.jpg")) 
+
+    precision = metrics.precision_score(y_true, y_pred)
+    recall = metrics.recall_score(y_true, y_pred)
+    f1 = metrics.f1_score(y_true, y_pred)
+    accuracy = metrics.accuracy_score(y_true, y_pred)
+
+    return precision, recall, f1, accuracy
 
 ## Run -----
-model = BertClassifier().to(device)
-optimizer = optim.Adam(model.parameters(), lr=2e-5)
+num_iters = 15
+precision_list = []
+recall_list = []
+f1_list = []
+accuracy_list = []
+for i in range(num_iters):
+    model = BertClassifier().to(device)
+    optimizer = optim.Adam(model.parameters(), lr=1e-5)
 
-train_model(model, optimizer)
+    base_file_name = str(i+1)
+    train_model(model, optimizer, base_file_name)
 
-# visualization
-train_loss_list, valid_loss_list, global_steps_list = load_metrics(os.path.join(destination_folder, "metrics.pt"))
+    # visualization
+    train_loss_list, valid_loss_list, global_steps_list = load_metrics(
+        os.path.join(destination_folder, base_file_name+"metrics.pt"))
+    
+    loss_fig = plt.figure()
+    plt.plot(global_steps_list, train_loss_list, label="Train")
+    plt.plot(global_steps_list, valid_loss_list, label="Valid")
+    plt.xlabel("Global steps")
+    plt.ylabel("Loss")
+    plt.legend()
 
-plt.plot(global_steps_list, train_loss_list, label="Train")
-plt.plot(global_steps_list, valid_loss_list, label="Valid")
-plt.xlabel("Global steps")
-plt.ylabel("Loss")
-plt.legend()
-plt.show()
+    plt.savefig(os.path.join(destination_folder, base_file_name+"loss.jpg"))
+    plt.show()
 
-best_model = BertClassifier().to(device)
-load_checkpoint(os.path.join(destination_folder, "model.pt"), best_model)
-evaluate(best_model, test_iter)
+    best_model = BertClassifier().to(device)
+    load_checkpoint(os.path.join(destination_folder, base_file_name+"model.pt"), best_model)
+    precision, recall, f1, accuracy = evaluate(best_model, base_file_name, test_iter)
+
+    precision_list.append(precision)
+    recall_list.append(recall)
+    f1_list.append(f1)
+    accuracy_list.append(accuracy)
+
+result_df = pd.DataFrame(data={"precision": precision_list, "recall": recall_list,
+                               "f1": f1_list, "accuracy": accuracy_list})
+result_df.to_excel(os.path.join(destination_folder, "bert-seq-clf.xlsx"))
+boxplot = result_df.boxplot()
