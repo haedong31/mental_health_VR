bf0bf6e39d388b03f3b592afef7bb5b0ff0c1ef3
Haedong Kim <haedong.kim90@gmail.com>
2021-05-15 20:32:52 -0400

    Minor edits

--- audio_preprocess.py
+++ audio_preprocess.py
@@ -1,24 +1,29 @@
 import os
+from pathlib import Path
 from glob import glob
 from pydub import AudioSegment
 
 import pandas as pd
-
 from sklearn.model_selection import train_test_split
 
-def multiple_mp3_to_wav(dir):
-    src_paths =  glob(dir + "/*.mp3")
-
+def multiple_mp3_to_wav(source_dir, out_dir):
+    out_dir = Path(out_dir)
+    os.makedirs(out_dir, exist_ok=True)
+    
+    src_paths =  glob(source_dir + "/*.mp3")
     for src in src_paths:
-        wo_ext = os.path.splitext(src)[0]
-        out_path =  wo_ext + ".wav"
+        src = Path(src)
+        
+        fname = src.name
+        fname = os.path.splitext(fname)[0]
+        fname = fname + '.wav'
         
         sound = AudioSegment.from_mp3(src)
-        sound.export(out_path, format="wav")
+        sound.export(out_dir/fname, format="wav")
 
 ## conver mp3 to wav
-multiple_mp3_to_wav("./data/audio/control/cookie")
-multiple_mp3_to_wav("./data/audio/dementia/cookie")
+multiple_mp3_to_wav("./data/audio_mp3/control/cookie", './data/audio/control')
+multiple_mp3_to_wav("./data/audio_mp3/dementia/cookie", './data/audio/dementia')
 
 ## split into train, valid, and test dataset -----
 # train:valid:test = 0.765:0.135:0.1
--- scratch.py
+++ scratch.py
@@ -1,16 +1,12 @@
+from pathlib import Path
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader
-from torch.utils.tensorboard import SummaryWriter
-
 import torchaudio
 import torchaudio.transforms as transforms
-from torchvision import models
 
 from bert_vocab_check import BertVocabCheck
 
@@ -41,9 +37,10 @@ exp_freq_vocab = exp_vocab.freq_vocab
 con_freq_vocab = dict(sorted(con_freq_vocab.items(), key=lambda x: x[1], reverse=True))
 exp_freq_vocab = dict(sorted(exp_freq_vocab.items(), key=lambda x: x[1], reverse=True))
 
-
-
 ## audio dataset -----
+
+
+
 config_dict = {"num_epochs": 6, "batch_size": 8, "drop_out": 0.25,
     "base_lr": 0.005, "num_mel_filters": 2048, "resample_freq": 22050}
 
