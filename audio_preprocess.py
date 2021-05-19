5a64c3f08c6761d06cc7130deccabe790ed0a6ac
Haedong Kim <haedong.kim90@gmail.com>
2021-05-19 16:05:04 -0400

    Audio data preprocessing

--- audio_preprocess.py
+++ audio_preprocess.py
@@ -1,5 +1,5 @@
 import os
-from pathlib import Path
+from pathlib2 import Path
 from glob import glob
 from pydub import AudioSegment
 
@@ -21,24 +21,28 @@ def multiple_mp3_to_wav(source_dir, out_dir):
         sound = AudioSegment.from_mp3(src)
         sound.export(out_dir/fname, format="wav")
 
-## conver mp3 to wav
+##### convert mp3 to wav -----
 multiple_mp3_to_wav("./data/audio_mp3/control/cookie", './data/audio/control')
 multiple_mp3_to_wav("./data/audio_mp3/dementia/cookie", './data/audio/dementia')
 
-## split into train, valid, and test dataset -----
+##### recurrence plot image augmentation -----
+
+
+##### split into train, valid, and test dataset -----
 # train:valid:test = 0.765:0.135:0.1
 train_test_ratio = 0.90
 train_valid_ratio = 0.85
 
 # control group
-con_paths = glob("./data/audio/control/*.wav")
-con_meta = pd.DataFrame(data={"file_paths": con_paths, "class": [0]*len(con_paths)})
+base_dir = Path('./data/audio')
+con_paths = glob(str(base_dir/'control'/'*.png'))
+con_meta = pd.DataFrame(data={"path": con_paths, "label": [0]*len(con_paths)})
 con_meta_train, con_meta_test = train_test_split(con_meta, train_size=train_test_ratio, random_state=1)
 con_meta_train, con_meta_valid = train_test_split(con_meta_train, train_size=train_valid_ratio, random_state=1)
 
 # experimental group (dementia)
-exp_paths = glob("./data/audio/dementia/*.wav")
-exp_meta = pd.DataFrame(data={"file_paths": exp_paths, "class": [1]*len(exp_paths)})
+exp_paths = glob(str(base_dir/'dementia'/'*.png'))
+exp_meta = pd.DataFrame(data={"path": exp_paths, "label": [1]*len(exp_paths)})
 exp_meta_train, exp_meta_test = train_test_split(exp_meta, train_size=train_test_ratio, random_state=1)
 exp_meta_train, exp_meta_valid = train_test_split(exp_meta_train, train_size=train_valid_ratio, random_state=1)
 
@@ -48,6 +52,6 @@ meta_valid = pd.concat([con_meta_valid, exp_meta_valid], ignore_index=True, sort
 meta_test = pd.concat([con_meta_test, exp_meta_test], ignore_index=True, sort=False).sample(frac=1)
 
 # save
-meta_train.to_csv("./data/audio/meta_train.csv", index=False)
-meta_valid.to_csv("./data/audio/meta_valid.csv", index=False)
-meta_test.to_csv("./data/audio/meta_test.csv", index=False)
+meta_train.to_csv(str(base_dir/'meta_train.csv'), index=False)
+meta_valid.to_csv(str(base_dir/'meta_valid.csv'), index=False)
+meta_test.to_csv(str(base_dir/'meta_test.csv'), index=False)
