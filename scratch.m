58857efd6ab2390b793fc3b4c612024e6432599b
Haedong Kim <haedong.kim90@gmail.com>
2022-07-13 14:08:56 -0400

    Debugging

--- scratch.m
+++ scratch.m
@@ -1,3 +1,44 @@
+%% HRQ debugging
+clc
+clearvars
+close all
+
+data_dir1 = fullfile(pwd,"data/hra/control/");
+data_dir2 = fullfile(pwd,"data/hra/dementia/");
+
+fnames_hc = dir(data_dir1);
+fnames_hc = fnames_hc(~ismember({fnames_hc.name},{'.','..'}));
+fnames_ad = dir(data_dir2);
+fnames_ad = fnames_ad(~ismember({fnames_ad.name},{'.','..'}));
+
+fsize1 = size(fnames_hc,1);
+fsize2 = size(fnames_ad,1);
+feat_mx = NaN(fsize1+fsize2,32*3+32*32*3);
+
+num_id = 32;
+alpha = 0.04;
+nbins = 0; % 0: auto
+order = 1;
+
+for i=1:fsize1
+
+    csig = readtable(fullfile(data_dir1,fnames_hc(i).name));
+    csig = csig.x + 1;
+    ifs_add = IFS(csig,num_id,alpha);
+    [feat_mx(i,1),feat_mx(i,2),feat_mx(i,3)] = heterorecurrence(...
+        ifs_add,csig,order,nbins);
+end
+
+for i=1:fsize2
+    csig = readtable(fullfile(data_dir2,fnames_ad(i).name));
+    csig = csig.x + 1;
+    ifs_add = IFS(csig,num_id,alpha);
+    [feat_mx(fsize1+i,1),feat_mx(fsize1+i,2),feat_mx(fsize1+i,3)] = heterorecurrence(...
+        ifs_add,csig,order,nbins);
+end
+
+
+%% IFS debugging
 clc
 clearvars
 close all
